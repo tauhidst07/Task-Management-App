6d751ff4ad94e4f11ff964ed79338e42955be7b5
tauhidst07 <atauhid07@gmail.com>
2024-10-03 20:20:13 +0530

    add styling to task component

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -4,22 +4,28 @@ import React, { useState } from 'react'
 const Tasks = ({taskProps}) => { 
     const [task,setTask] = useState(taskProps);
   return (
-    <div>
-      <h1>{task.title}</h1> 
-      <h2>{task.description}</h2> 
-      <p>status: {task.status} </p> 
-      <p>priority: {task.priority}</p> 
-      <p>deadline: {task.deadline}</p> 
-      <button onClick={async()=>{
-         const res = await axios.put(`http://localhost:3000/api/v1/task/${task._id}`,{
-            status:"completed"
-          },{
-            headers:{
-                authorization:`Bearer ${localStorage.getItem("token")}`
-            } 
-          }) 
-          setTask(res.data.updatedTask);
-      }}>mark as complete</button>
+    <div className=' rounded-md flex flex-col  my-2 md:w-[500px] p-3 m-3 shadow-md'> 
+      <div className='ml-4'>
+          <h1 className='text-3xl font-semibold'>{task.title}</h1> 
+          <h2>{task.description}</h2> 
+          <p>status: {task.status} </p> 
+          <p>priority: {task.priority}</p> 
+          <p>deadline: {task.deadline}</p> 
+
+      </div>
+      <div className='flex w-full ml-5 '> 
+        <button onClick={async()=>{
+          const res = await axios.put(`http://localhost:3000/api/v1/task/${task._id}`,{
+              status:"completed"
+            },{
+              headers:{
+                  authorization:`Bearer ${localStorage.getItem("token")}`
+              } 
+            }) 
+            setTask(res.data.updatedTask);
+        }} className='bg-gray-700 py-2 px-3 w-max rounded-md  text-white'>mark as complete</button>
+      </div> 
+      
     </div>
   )
 }
